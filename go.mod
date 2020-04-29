1f805ef08b5c57632a8e5cc80889052224ce6a45
Oliver Coad <oliver.coad@gmail.com>
2020-04-29 23:43:59 +1000

    Add maxwidth and maxheight to resize jpegs

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.14
 require (
 	github.com/gabriel-vasile/mimetype v1.1.0
 	github.com/logrusorgru/aurora v0.0.0-20200102142835-e9ef32dff381
+	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646
 	github.com/urfave/cli/v2 v2.2.0
 )
--- src/wcat/main.go
+++ src/wcat/main.go
@@ -2,21 +2,46 @@ package main
 
 import (
 	"fmt"
+	"image/jpeg"
+	"io"
 	"io/ioutil"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"path/filepath"
 	"strings"
 
 	"github.com/gabriel-vasile/mimetype"
+	"github.com/nfnt/resize"
 
 	"github.com/logrusorgru/aurora"
 	"github.com/urfave/cli/v2"
 )
 
+func resizeImage(f *os.File, maxWidth, maxHeight int) (*io.PipeReader, error) {
+	img, err := jpeg.Decode(f)
+	if err != nil {
+		return nil, err
+	}
+	if maxWidth <= 0 {
+		maxWidth = math.MaxInt32
+	}
+	if maxHeight <= 0 {
+		maxHeight = math.MaxInt32
+	}
+	mW, mH := uint(maxWidth), uint(maxHeight)
+	resized := resize.Thumbnail(mW, mH, img, resize.Lanczos3)
+	outr, outw := io.Pipe()
+	go func() {
+		defer outw.Close()
+		jpeg.Encode(outw, resized, nil)
+	}()
+	return outr, nil
+}
+
 // PreviewFile posts the file to the wcat server with filename and Content-Type headers.
-func PreviewFile(client *http.Client, wcatserver string, filename string) {
+func PreviewFile(client *http.Client, wcatserver string, filename string, maxwidth, maxheight int) {
 	fmt.Print("Preview file ", filename, " ... ")
 
 	f, err := os.Open(filename)
@@ -33,7 +58,18 @@ func PreviewFile(client *http.Client, wcatserver string, filename string) {
 		return
 	}
 	f.Seek(0, 0)
-	req, err := http.NewRequest("POST", wcatserver+"/api/showthis", f)
+	var requestBody io.Reader = f
+	if contentType.Is("image/jpeg") {
+		resizedBodyReader, err := resizeImage(f, maxwidth, maxheight)
+		if err != nil {
+			fmt.Println(aurora.Red(err))
+			return
+		}
+		defer resizedBodyReader.Close()
+		requestBody = resizedBodyReader
+	}
+
+	req, err := http.NewRequest("POST", wcatserver+"/api/showthis", requestBody)
 	if err != nil {
 		fmt.Println(aurora.Red("Error making request"), err)
 		return
@@ -75,14 +111,28 @@ func main() {
 				Usage:   "Post previews to `SERVER`",
 				EnvVars: []string{"WCATSERVER"},
 			},
+			&cli.IntFlag{
+				Name:    "maxwidth",
+				Aliases: []string{"mw"},
+				Value:   1024,
+				Usage:   "the max width for images. 0 for no max width",
+			},
+			&cli.IntFlag{
+				Name:    "maxheight",
+				Aliases: []string{"mh"},
+				Value:   800,
+				Usage:   "the max height for images. 0 for no max height",
+			},
 		},
 		Action: func(c *cli.Context) error {
 			if c.NArg() >= 1 {
 				client := &http.Client{}
 				wcatserver := c.String("server")
+				maxwidth := c.Int("maxwidth")
+				maxheight := c.Int("maxheight")
 				for i := 0; i < c.NArg(); i++ {
 					filename := c.Args().Get(i)
-					PreviewFile(client, wcatserver, filename)
+					PreviewFile(client, wcatserver, filename, maxwidth, maxheight)
 				}
 			} else {
 				fmt.Println("No files specified")
